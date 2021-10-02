152c6e1ba223c6ce7559a398db687512f98499c3
CrappyDill <mddillon47@gmail.com>
2021-10-01 19:30:27 -0700

    more jest matchers

--- matchers.test.js
+++ matchers.test.js
@@ -1,10 +1,22 @@
 describe("test matchers", function () {
-  test("compare toBe and toEqual", function () {
-    const nums = [2, 3, 4];
-    const copy = [...nums];
-    const nums2 = nums;
-    expect(copy).toEqual(nums);
-    // expect(copy).toBe(nums) not same reference
-    expect(nums2).toBe(nums);
-  });
+    test("compare toBe and toEqual", function () {
+        const nums = [2, 3, 4];
+        const copy = [...nums];
+        const nums2 = nums;
+
+        expect(copy).toEqual(nums);
+        // expect(copy).toBe(nums) not same reference
+        expect(nums2).toBe(nums);
+    });
+    
+    test("playing with toContain", function () {
+        const colors = ["red", "green"];
+
+        expect(colors).toContain("red");
+        expect("hello").toContain("hell");
+    });
+    
+    test("toBeGreaterThan", function () {
+        expect(4).toBeGreaterThan(2)
+    })
 });
