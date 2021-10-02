4f1125b1ab912df0316a839181ac5028c2449837
CrappyDill <mddillon47@gmail.com>
2021-10-01 19:40:41 -0700

    jest any

--- matchers.test.js
+++ matchers.test.js
@@ -1,22 +1,28 @@
 describe("test matchers", function () {
-    test("compare toBe and toEqual", function () {
-        const nums = [2, 3, 4];
-        const copy = [...nums];
-        const nums2 = nums;
+  test("compare toBe and toEqual", function () {
+    const nums = [2, 3, 4];
+    const copy = [...nums];
+    const nums2 = nums;
 
-        expect(copy).toEqual(nums);
-        // expect(copy).toBe(nums) not same reference
-        expect(nums2).toBe(nums);
-    });
-    
-    test("playing with toContain", function () {
-        const colors = ["red", "green"];
+    expect(copy).toEqual(nums);
+    // expect(copy).toBe(nums) not same reference
+    expect(nums2).toBe(nums);
+  });
 
-        expect(colors).toContain("red");
-        expect("hello").toContain("hell");
-    });
-    
-    test("toBeGreaterThan", function () {
-        expect(4).toBeGreaterThan(2)
-    })
+  test("playing with toContain", function () {
+    const colors = ["red", "green"];
+
+    expect(colors).toContain("red");
+    expect("hello").toContain("hell");
+  });
+
+  test("toBeGreaterThan", function () {
+    expect(4).toBeGreaterThan(2);
+  });
+
+  test("any", function () {
+    const randNum = Math.random() * 3;
+    expect(randNum).toEqual(expect.any(Number));
+    expect("crappydill").toEqual(expect.any(String));
+  });
 });
